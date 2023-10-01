5138e49932116941d776ccaaab55a51ab7c1da83
Tobias J <tobias.jaks@gmail.com>
2023-10-01 13:04:21 +0200

    added dunamic meal planner form. now trying to access dynamic data from form to list of objects

---
+++ Models/RecipeRequestEasyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganizerBlazor.Models
+{
+    public class RecipeRequestEasyDTO
+    {
+        public int Portions { get; set; } = 1;
+        public string Difficulty = "any";
+        public string Category = "any";
+        public int MaxCookTime { get; set; } = 120;
+
+    }
+}

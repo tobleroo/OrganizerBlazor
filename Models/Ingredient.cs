5374aff9cb90aebd44c691edc4b37a138d81c22f
Tobleroo <tobias.jaks@gmail.com>
2023-10-09 10:56:18 +0200

    change ingredient amount data type to double from int

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -2,6 +2,6 @@ public class Ingredient
 {
     public Guid Guid { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = "new ingredient";
-    public int? Quantity { get; set; }
+    public double? Quantity { get; set; }
     public string? Unit { get; set; }
 }

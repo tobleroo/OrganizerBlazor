4b1ee7b820a9e9369d7d4f4510a6fe8be0ab66cf
Tobias J <tobias.jaks@gmail.com>
2023-12-11 19:57:46 +0100

    started on todo feature

---
+++ Todo/Models/TodoActivity.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OrganizerBlazor.Todo.Models
+{
+    public class TodoActivity
+    {
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Title { get; set; }
+
+        public int FrequencyAmount { get; set; } = 0;
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public FrequencyTypes FrequencyType { get; set; }
+
+        public List<string> DatesWhenCompleted { get; set; } = new List<string>();
+    }
+}

46ec2ed62186f85221162509df6c4d55ec3a0fbe
Tobias J <tobias.jaks@gmail.com>
2023-12-20 16:51:34 +0100

    cookbook shoppinglist upgraded to checkboxes

---
+++ Cookbook/Models/AdditionalFoodCurrentItem.cs
@@ -0,0 +1,8 @@
+﻿namespace OrganizerBlazor.Cookbook.Models
+{
+    public class AdditionalFoodCurrentItem
+    {
+        public string Name { get; set; } = "not set";
+        public bool Completed { get; set; } = false;
+    }
+}
--- Cookbook/Models/SingleShopList.cs
+++ Cookbook/Models/SingleShopList.cs
@@ -5,7 +5,7 @@
         public string ListName { get; set; } = "not set";
         public List<SingleShopListRecipe> SingleShopListRecipes { get; set; } = new List<SingleShopListRecipe>();
 
-        public List<string> AdditionalItems { get; set; } = new List<string>();
+        public List<AdditionalFoodCurrentItem> AdditionalItems { get; set; } = new List<AdditionalFoodCurrentItem>();
 
     }
 }

268b96d20bcce3dd5886dc12870beea6e2012096
Tobias J <tobias.jaks@gmail.com>
2023-12-23 10:12:34 +0100

    added fooditem class

--- Cookbook/Models/CookBookDTO/ShoppingListPageDTO.cs
+++ Cookbook/Models/CookBookDTO/ShoppingListPageDTO.cs
@@ -5,7 +5,7 @@ namespace OrganizerBlazor.Cookbook.Models.CookBookDTO
     public class ShoppingListPageDTO
     {
         public SingleShopList? SingleShopList { get; set; }
-        public List<string>? AdditionalItems { get; set; }
+        public List<AdditionalFoodCurrentItem>? AdditionalItems { get; set; }
 
         public List<string>? RecommendedAdditionalItems { get; set; } = new();
     }
